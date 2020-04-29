966af5da5891dd2466cd72c2e58ddc4e1142a9f8
theamazingeagle <oryolman@gmail.com>
2020-04-29 15:24:34 +0300

    docker dev workspace

---
+++ postgres/schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE switter;
+USE switter;
+CREATE TABLE users(
+    user_id SERIAL NOT NULL PRIMARY KEY,
+    user_name VARCHAR(256),
+    user_email VARCHAR(256),
+    user_password VARCHAR(256)
+);
+CREATE TABLE messages(
+    message_id SERIAL NOT NULL PRIMARY KEY,
+    message_url VARCHAR(2048),
+    message_userid BIGINT,
+    message_date TIMESTAMP,
+    message_rootid VARCHAR(256),
+    message_text TEXT
+);
+
+ALTER TABLE messages ADD FOREIGN KEY (message_userid) REFERENCES users(user_id);
