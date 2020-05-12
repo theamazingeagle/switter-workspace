22f1002d42daf34b4431031c765134d947350ebd
theamazingeagle <oryolman@gmail.com>
2020-05-13 01:12:07 +0300

    fix postgres-container settings

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE switter;
-USE switter;
+--CREATE DATABASE switter;--
+--USE switter;--
 CREATE TABLE users(
     user_id SERIAL NOT NULL PRIMARY KEY,
     user_name VARCHAR(256),
@@ -11,8 +11,9 @@ CREATE TABLE messages(
     message_url VARCHAR(2048),
     message_userid BIGINT,
     message_date TIMESTAMP,
-    message_rootid VARCHAR(256),
+    --message_rootid VARCHAR(256),--
     message_text TEXT
 );
 
 ALTER TABLE messages ADD FOREIGN KEY (message_userid) REFERENCES users(user_id);
+--alter table messages drop column message_rootid;--
