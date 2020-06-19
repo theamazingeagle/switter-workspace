c08b03b9920bc60f19b2f632e7fe5f9f1b936e89
theamazingeagle <oryolman@gmail.com>
2020-06-19 22:20:56 +0300

    Login, Register endpoints

--- switter-back/src/switter-back/router/router.go
+++ switter-back/src/switter-back/router/router.go
@@ -1,19 +1,19 @@
 package router
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-
-	//"strconv"
-	"fmt"
-	sql "switter-back/sql"
+	"strings"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
-
-	//"github.com/theamazingeagle/switter-back/types"
+	//"strconv"
 	/* ------ */
-	"encoding/json"
+	sql "switter-back/sql"
+	"switter-back/types"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 var (
@@ -31,7 +31,16 @@ type Claims struct {
 func accessMiddleWare(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		/* some checks? */
-		handler.ServeHTTP(w, r)
+		trustedRoutes := []string{
+			"/api/login",
+			"/api/register",
+		}
+		for _, route := range trustedRoutes {
+			if route == r.URL.RequestURI() {
+				handler.ServeHTTP(w, r)
+			}
+		}
+
 	})
 }
 
@@ -39,7 +48,7 @@ func accessMiddleWare(handler http.Handler) http.Handler {
 func generateAccessTokenString(userEmail string, userPassword string, signingKey []byte) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &Claims{
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Duration(expTime) * time.Millisecond).Unix(),
+			ExpiresAt: time.Now().Add(time.Duration(expTime) * time.Second).Unix(),
 			IssuedAt:  time.Now().Unix(),
 		},
 		Email:    userEmail,
@@ -54,20 +63,57 @@ func generateAccessTokenString(userEmail string, userPassword string, signingKey
 
 //
 func Login(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		w.Write([]byte("no data"))
+	} else {
+		//log.Println("route:Login:FormValue: ", r.Form)
+		email := strings.TrimSpace(r.FormValue("userEmail"))
+		password := strings.TrimSpace(r.FormValue("userPassword"))
+		user := &types.User{}
 
-	log.Printf("%+v", r.Form)
-	//w.Write([]byte(tokenString))
-	//return //What???
+		if len(email) > 0 && len(password) > 0 {
+			user = sql.GetUserByEmail(email)
+		}
+		if user != nil {
+			if user.Email == email && user.Password == password {
+				accessKey, err := generateAccessTokenString(email, password, signingKey)
+				if err != nil {
+					w.Write([]byte("internal error"))
+				}
+				w.Write([]byte(accessKey))
+			}
+		}
+	}
 }
 
 //
 func Register(w http.ResponseWriter, r *http.Request) {
-
-}
-
-//
-func RefreshToken(w http.ResponseWriter, r *http.Request) {
-
+	err := r.ParseForm()
+	if err != nil {
+		w.Write([]byte("no data"))
+	} else {
+		//log.Println("route:Login:FormValue: ", r.Form)
+		email := strings.TrimSpace(r.FormValue("userEmail"))
+		password := strings.TrimSpace(r.FormValue("userPassword"))
+		username := strings.TrimSpace(r.FormValue("userName"))
+		if len(email) > 0 && len(password) > 0 && len(username) > 0 {
+			if sql.GetUserByEmail(email) == nil {
+				if sql.CreateUser(username, password, email) != nil {
+					w.Write([]byte("internal error"))
+				} else {
+					//send accessToken
+					accessKey, err := generateAccessTokenString(email, password, signingKey)
+					if err != nil {
+						w.Write([]byte("generate access token error"))
+					}
+					w.Write([]byte(accessKey))
+				}
+			} else {
+				w.Write([]byte("user exist"))
+			}
+		}
+	}
 }
 
 //
@@ -92,6 +138,7 @@ func Start(host string, port int) {
 	mux.HandleFunc("/api/getmessages", GetMessageList)
 	//mux.HandleFunc("/api/", )
 	mux.HandleFunc("/api/login", Login)
+	mux.HandleFunc("/api/register", Register)
 	//mux.HandleFunc("/api/refreshtoken", )
 	requestHandler := accessMiddleWare(mux)
 	//log.Fatal(http.ListenAndServe(host+":"+strconv.Itoa(port), nil))
--- switter-back/src/switter-back/sql/user.go
+++ switter-back/src/switter-back/sql/user.go
@@ -1,15 +1,19 @@
 package sql
 
 import (
+	"fmt"
 	"log"
 	"switter-back/types"
 )
 
-func CreateUser(userName, password, email string) {
+// CreateUser
+func CreateUser(userName, password, email string) error {
 	_, err := dbConn.Exec("INSERT INTO users(user_name,user_password,user_email) VALUES($1, $2, $3)", userName, password, email)
 	if err != nil {
 		log.Println("sql.CreateUser err: ", err)
+		return fmt.Errorf("sql.CreateUser Error: ", err)
 	}
+	return nil
 }
 func GetUser(ID int64) *types.User {
 	row := dbConn.QueryRow("SELECT * FROM users WHERE user_id=$1", ID)
@@ -26,9 +30,14 @@ func GetUserByEmail(email string) *types.User {
 	row := dbConn.QueryRow("SELECT * FROM users WHERE user_email=$1", email)
 	if row == nil {
 		log.Println("sql.GetUserByEmail err: ", row)
+		return nil
 	}
 	user := &types.User{}
-	row.Scan(&user.ID, &user.UserName, &user.Password, &user.Email)
+	err := row.Scan(&user.ID, &user.UserName, &user.Password, &user.Email)
+	if err != nil {
+		log.Println("sql.GetUserByEmail error, no row: ", err)
+		return nil
+	}
 	log.Println("sql.GetUserByEmail result: ", user)
 	return user
 }
